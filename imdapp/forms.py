98ed246cd93189eb8d418fd62d886faa7962152d
Monika <monikagavate5@gmail.com>
2022-10-20 15:59:10 +0530

    commit

---
+++ imdapp/forms.py
@@ -0,0 +1,200 @@
+from django import forms
+from django.forms import formset_factory
+from .models import (
+    Supplier,
+    PurchaseBill,
+    PurchaseItem,
+    PurchaseBillDetails,
+    SaleBill,
+    SaleItem,
+    SaleBillDetails, Stock
+)
+class StockForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):                                                        # used to set css classes to the various fields
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only'})
+        self.fields['category'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['subcategory'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['type'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['quantity'].widget.attrs.update({'class': 'textinput form-control', 'min': '0'})
+        self.fields['Mode_of_delivery'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['condition'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['label_code'].widget.attrs.update({'class': 'textinput form-control'})
+
+
+
+
+
+
+    class Meta:
+        model = Stock
+        fields = ['name','category','subcategory','type','description','quantity','Mode_of_delivery','condition','label_code']
+        # widgets = {
+        #     'description': forms.Textarea(
+        #         attrs={
+        #             'class': 'textinput form-control',
+        #             'rows': '2'
+        #         }
+        #     )
+        # }
+
+
+# form used to select a supplier
+class SelectSupplierForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['supplier'].queryset = Supplier.objects.filter(is_deleted=False)
+        self.fields['supplier'].widget.attrs.update({'class': 'textinput form-control'})
+    class Meta:
+        model = PurchaseBill
+        fields = ['supplier']
+
+# form used to render a single stock item form
+class PurchaseItemForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['stock'].queryset = Stock.objects.filter(is_deleted=False)
+        self.fields['stock'].widget.attrs.update({'class': 'textinput form-control setprice stock', 'required': 'true'})
+        self.fields['quantity'].widget.attrs.update({'class': 'textinput form-control setprice quantity', 'min': '0', 'required': 'true'})
+        self.fields['perprice'].widget.attrs.update({'class': 'textinput form-control setprice price', 'min': '0', 'required': 'true'})
+    class Meta:
+        model = PurchaseItem
+        fields = ['stock', 'quantity', 'perprice']
+
+# formset used to render multiple 'PurchaseItemForm'
+PurchaseItemFormset = formset_factory(PurchaseItemForm, extra=1)
+
+# form used to accept the other details for purchase bill
+class PurchaseDetailsForm(forms.ModelForm):
+    class Meta:
+        model = PurchaseBillDetails
+        fields = ['total']
+
+
+# form used for supplier
+class SupplierForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only'})
+        self.fields['phone'].widget.attrs.update({'class': 'textinput form-control', 'maxlength': '10', 'pattern' : '[0-9]{10}', 'title' : 'Numbers only'})
+        self.fields['email'].widget.attrs.update({'class': 'textinput form-control'})
+
+    class Meta:
+        model = Supplier
+        fields = ['name', 'phone', 'address', 'email']
+        widgets = {
+            'address' : forms.Textarea(
+                attrs = {
+                    'class' : 'textinput form-control',
+                    'rows'  : '2'
+                }
+            )
+        }
+
+
+
+
+# form used to get customer details
+class SaleForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only', 'required': 'true'})
+        self.fields['phone'].widget.attrs.update({'class': 'textinput form-control', 'maxlength': '10', 'pattern' : '[0-9]{10}', 'title' : 'Numbers only', 'required': 'true'})
+        self.fields['email'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['address'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only', 'required': 'true'})
+        self.fields['issued_to'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only', 'required': 'true'})
+        self.fields['category'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['subcategory'].widget.attrs.update({'class': 'textinput form-control'})
+        # self.fields['type'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['quantity'].widget.attrs.update({'class': 'textinput form-control', 'min': '0'})
+        self.fields['Mode_of_delivery'].widget.attrs.update({'class': 'textinput form-control'})
+        self.fields['label_code'].widget.attrs.update({'class': 'textinput form-control'})
+
+
+
+    class Meta:
+        model = SaleBill
+        fields = ['name', 'phone', 'email','address','issued_to','category','subcategory','quantity','Mode_of_delivery','label_code','description']
+        # widgets = {
+        #     'address' : forms.Textarea(
+        #         attrs = {
+        #             'class' : 'textinput form-control',
+        #             'rows'  : '2'
+        #         }
+        #     )
+        # }
+
+
+        #     'description': forms.Textarea(
+        #         attrs={
+        #             'class': 'textinput form-control',
+        #             'rows': '2'
+        #         }
+        #
+        #     )
+        # }
+
+# form used to render a single stock item form
+# class PItemForm(forms.ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['stock'].queryset = Stock.objects.filter(is_deleted=False)
+#         self.fields['stock'].widget.attrs.update({'class': 'textinput form-control setprice stock', 'required': 'true'})
+#         self.fields['quantity'].widget.attrs.update({'class': 'textinput form-control setprice quantity', 'min': '0', 'required': 'true'})
+#         self.fields['perprice'].widget.attrs.update({'class': 'textinput form-control setprice price', 'min': '0', 'required': 'true'})
+#
+#     class Meta:
+#         model = PItem
+#         fields = ['stock', 'quantity', 'perprice']
+#
+# # formset used to render multiple 'SaleItemForm'
+# PItemFormset = formset_factory(PItemForm, extra=1)
+#
+# # form used to accept the other details for sales bill
+# class PDetailsForm(forms.ModelForm):
+#     class Meta:
+#         model = PBillDetails
+#         fields = ['total']
+#
+class SaleItemForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['stock'].queryset = Stock.objects.filter(is_deleted=False)
+        self.fields['stock'].widget.attrs.update({'class': 'textinput form-control setprice stock', 'required': 'true'})
+        self.fields['quantity'].widget.attrs.update({'class': 'textinput form-control setprice quantity', 'min': '0', 'required': 'true'})
+        self.fields['perprice'].widget.attrs.update({'class': 'textinput form-control setprice price', 'min': '0', 'required': 'true'})
+
+    class Meta:
+        model = SaleItem
+        fields = ['stock', 'quantity', 'perprice']
+
+# formset used to render multiple 'SaleItemForm'
+SaleItemFormset = formset_factory(SaleItemForm, extra=1)
+
+# form used to accept the other details for sales bill
+class SaleDetailsForm(forms.ModelForm):
+    class Meta:
+        model = SaleBillDetails
+        fields = ['total']
+
+
+
+
+#change
+# class CategoryForm(forms.ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['category'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only', 'required': 'true'})
+#         self.fields['subcategory'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z\s]{1,50}', 'title' : 'Alphabets and Spaces only', 'required': 'true'})
+#         # self.fields['product'].widget.attrs.update({'class': 'textinput form-control', 'pattern' : '[a-zA-Z]', 'title' : 'Alphabets and Spaces only', 'required': 'true'})
+#         # self.fields['type'].widget.attrs.update({'class': 'textinput form-control'})
+#
+#
+#     class Meta:
+#         model = Category
+#         fields = ['category','subcategory']
+#
+class ChoiceField:
+    pass
