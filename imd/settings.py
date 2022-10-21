b78ec89897ca488a9710f921d18eb8055e277146
Monika <monikagavate5@gmail.com>
2022-10-21 16:29:24 +0530

    dome

--- imd/settings.py
+++ imd/settings.py
@@ -129,16 +129,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# )
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 STATISTICS_DIRS=(
-      os.path.join(BASE_DIR,'static')
+      os.path.join(BASE_DIR,'imd/homepage/static')
  )
-STATIC_ROOT=BASE_DIR, 'static'
+STATIC_ROOT=BASE_DIR, 'imd/homepage/static'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = 'imd/homepage/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
