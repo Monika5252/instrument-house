d395931c836aa44eb120f39f1558033f2d583fee
Monika <monikagavate5@gmail.com>
2022-10-21 16:46:54 +0530

    code

--- imd/urls.py
+++ imd/urls.py
@@ -22,10 +22,11 @@ urlpatterns = [
 
     path('admin/', admin.site.urls, name='admin'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+
 
     path('', include('homepage.urls')),
     path('inventory/', include('imdapp.urls')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     # path('transactions/', include('transactions.urls')),
     # path('inventory/',include('imdapp.urls')),
 ]
