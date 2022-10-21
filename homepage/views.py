62f750ee06bbc25af6861a1031aea1c5f8e720e6
Monika <monikagavate5@gmail.com>
2022-10-21 18:14:06 +0530

    commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
-from imd.imdapp.models import *
+
 
 
 
@@ -8,12 +8,15 @@ class HomeView(View):
     template_name = "home.html"
     def get(self, request):        
         labels = []
-        data = []        
+        data = []
+        from imd.imdapp.models import Stock
         stockqueryset = Stock.objects.filter(is_deleted=False).order_by('-quantity')
         for item in stockqueryset:
             labels.append(item.name)
             data.append(item.quantity)
+        from imd.imdapp.models import SaleBill
         sales = SaleBill.objects.order_by('-time')[:3]
+        from imd.imdapp.models import PurchaseBill
         purchases = PurchaseBill.objects.order_by('-time')[:3]
         context = {
             'labels'    : labels,
