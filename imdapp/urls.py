c97c72e0c27257ea8c15662964ff42a84fd494a6
Monika <monikagavate5@gmail.com>
2022-10-25 17:08:14 +0530

    initial

--- imdapp/urls.py
+++ imdapp/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+
     path('', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
 
