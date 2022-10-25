c7b0f2730b04dbde909e660c69b939d50c1eff2f
Monika <monikagavate5@gmail.com>
2022-10-25 16:30:25 +0530

    commit

--- homepage/views.py
+++ homepage/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 
 
-#
 
-from imd.imdapp.models import SaleBill, PurchaseBill, Stock
 
 
 class HomeView(View):
