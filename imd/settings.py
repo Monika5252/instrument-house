43519611b5ad4892eeec1300b4f92026adc378a9
Monika <monikagavate5@gmail.com>
2022-10-25 14:37:14 +0530

    done

--- imd/settings.py
+++ imd/settings.py
@@ -147,7 +147,7 @@ STATISTICS_DIRS=(
       os.path.join(BASE_DIR,'static')
  )
 STATIC_ROOT=BASE_DIR, 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
