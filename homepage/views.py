d8dc3daadaddfa443340efcf2f5b1c9b519dbfaf
Monika <monikagavate5@gmail.com>
2022-11-03 10:30:18 +0530

    commit

--- homepage/views.py
+++ homepage/views.py
@@ -11,7 +11,7 @@ class HomeView(View):
     def get(self, request):
         labels = []
         data = []
-        stockqueryset = Stock.objects.filter(is_deleted=False).order_by('-name')
+        stockqueryset = Stock.objects.filter(is_deleted=False).order_by('-quantity')
         for item in stockqueryset:
             labels.append(item.name)
             data.append(item.quantity)
