7eb59cccb7a2dedeae48a08935da766860c9bfda
Monika <monikagavate5@gmail.com>
2022-11-03 18:34:39 +0530

    commit

--- imd/settings.py
+++ imd/settings.py
@@ -92,18 +92,18 @@ WSGI_APPLICATION = 'imd.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inventorydata',
-        'USER': 'inventory1',
-        'PASSWORD': 'inventory',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'inventorydata',
+    #     'USER': 'inventory1',
+    #     'PASSWORD': 'inventory',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
